4ce47bd7d9cdd582503f91538b3e6c98e27cf20c
Jochem Broekhoff <broekhoff.jochem@gmail.com>
2017-12-04 17:11:56 +0100

    Update to MC 1.12.2. Updated CDM dependencies to latest core build (143142ded30f4ae00b8bf2e10c91755d1abd89b2).

--- src/main/java/nl/jochembroekhoff/cdmlloaderdemo/Reference.java
+++ src/main/java/nl/jochembroekhoff/cdmlloaderdemo/Reference.java
@@ -7,6 +7,6 @@ public class Reference {
     public static final String MOD_ID = "cdmlloader";
     public static final String MOD_NAME = "CDMLLoader";
     public static final String MOD_VERSION = "0.0.1";
-    public static final String MC_VERSION = "[1.11,1.11.2]";
+    public static final String MC_VERSION = "[1.12.2]";
     public static final String DEPENDS = "required-after:cdm@[0.2.0,)";
 }
